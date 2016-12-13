d64b601ddfed71f84221b8e41cfb81f07df49d1a
Bittrance <anders.qvist@burtcorp.com>
2016-12-13 09:36:26 +0100

    Add lockfile to avoid conflicting uploads

--- config/scripts/forward-to-s3.sh
+++ config/scripts/forward-to-s3.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
-# TODO: Ensure that our stdout/err ends up in log file
+set -e
+
 # TODO: If there is already an upload of this file ongoing, kill it and restart
 
-die() { echo "$1" ; exit 1 ; }
+die() { logger -st sftp-bridge-forward "$1" ; exit 1 ; }
+
+LOCKDIR=/var/lock/forward-to-s3
 
 while getopts "b:p:" opt ; do
   case "$opt" in
-    b) S3_BUCKET="$OPTARG" ;;
+    b) S3_PREFIX="$OPTARG" ;;
     p) UPLOADED_PATH="$OPTARG" ;;
     *) die "Unknown parameter $opt" ;;
   esac
 done
 
-[ -n "$S3_BUCKET" ] || die "Missing parameter -b <bucket>"
+[ -n "$S3_PREFIX" ] || die "Missing parameter -b <s3_prefix>"
 [ -n "$UPLOADED_PATH" ] || die "Missing parameter -p <uploaded file>"
 
-uploadfile="$(basename $UPLOADED_PATH)"
-uploadfile="$(echo $uploadfile | tr -dC 'A-Za-z0-9_')"
+uploadfile=$(basename "$UPLOADED_PATH")
+uploadfile=$(echo "$uploadfile" | tr -dC 'A-Za-z0-9_.')
+target=${S3_PREFIX}/${uploadfile}
+lock=${LOCKDIR}/$(echo "$target" | base64)
 
 sleep 1
 
-# aws s3 cp $UPLOADED_PATH "$SFTPBRIDGE_UPLOAD_PREFIX/$prefix/$uploadfile"
-rm -f $UPLOADED_PATH
+if [ -f $lock ] ; then
+  pid=$(cat $lock)
+  kill $pid || true
+  rm $lock
+fi
+trap "rm $lock" SIGTERM EXIT
+echo $$ > $lock
+
+retries=10
+until aws s3 cp "$UPLOADED_PATH" "$target" ; do
+  if (( retries == 0 )) ; then die "Failed to upload $UPLOADED_PATH to $target" ; fi
+  retries=$(( retries - 1 ))
+  sleep 60
+done
+rm -f "$UPLOADED_PATH"
 
 logger -t sftp-bridge-forward "Uploaded ${uploadfile}"
